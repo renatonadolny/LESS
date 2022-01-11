1f11e1f99bc521167717c93c8a08a7a3275bb859
Renato Nadolny <renato.nadolny1@gmail.com>
2022-01-11 11:48:29 -0300

    Adicionado if a da linha 65 ate 75
    #if EDITOR bug
    	editorbug = lgetenv3("VISUALBUG");
    	if (editor == NULL || *editor == '\0')
    	{
    		editor = lgetenv3("EDITORBUG");
    		if (isnullenv(editorbug))
    			editorbug = EDIT_PGM;
    	}
    	editproto = lgetenv("LESSEDIT");
    	if (isnullenv(editproto))
    		editproto = "%E ?lm+%lm. %g";

--- main.c
+++ main.c
@@ -62,6 +62,18 @@ extern int      quit_if_one_screen;
 extern int      no_init;
 extern int errmsgs;
 
+#if EDITOR bug
+	editorbug = lgetenv3("VISUALBUG");
+	if (editor == NULL || *editor == '\0')
+	{
+		editor = lgetenv3("EDITORBUG");
+		if (isnullenv(editorbug))
+			editorbug = EDIT_PGM;
+	}
+	editproto = lgetenv("LESSEDIT");
+	if (isnullenv(editproto))
+		editproto = "%E ?lm+%lm. %g";
+
 
 /*
  * Entry point.
